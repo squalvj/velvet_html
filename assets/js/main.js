7b90168b98b185fedbee860edcebb2a8e503321d
Table Six <tablesix@ZARATHUSTRA.local>
2018-02-08 15:12:51 +0700

    fix navbar img hoverable

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,7 +1,7 @@
-$(document).ready(function() {
- 		
+var w = $(window).width();
+// $(document).ready(function() {
  
-});
+// });
 
 $(window).bind("load", function() {
   // init();
@@ -10,6 +10,29 @@ $(window).bind("load", function() {
   muncul();
 });
 
+// responsive function
+if(w > 756){
+	$(".hoverable").hover(function() {
+		var span = $(this).find('span')
+		var show = function(){
+			var img = span.find('img');
+			img.velocity({opacity:1},{duration:250},[0.165, 0.84, 0.44, 1])
+		}
+		span.velocity({display:'inline-flex',marginLeft:10,width:0,opacity:0},{duration:50})
+		.velocity({width:90,opacity:1},{duration:250,complete:show},[0.165, 0.84, 0.44, 1])
+	}, function() {
+		var span = $(this).find('span')
+		var hide = function(){
+			var img = span.find('img');
+			img.velocity({opacity:0},{duration:250},[0.165, 0.84, 0.44, 1])
+		}
+		span.velocity({width:0,opacity:0,marginLeft:0},{duration:250,complete:hide},[0.165, 0.84, 0.44, 1])
+		.velocity({display:'none',width:0,opacity:0},{duration:50})
+	});
+}else{
+
+}
+
 function muncul(){
 	$(".content, .nav-item").velocity({opacity:1},{duration:500,delay:1000})
 }
@@ -17,8 +40,6 @@ function muncul(){
 function init(){
 	
 }
-
-
 
 function InitCarouselHome(){
 	if ($(".hero-carousel").length){
@@ -69,29 +90,11 @@ function initSticky(){
 	}
 }
 
-function showImgNav(){
-	//var img = el.find('img');
-	//img.velocity({opacity:1},{duration:250},[0.165, 0.84, 0.44, 1])
-	console.log('complete')
-}
 
-$(".hoverable").hover(function() {
-	var span = $(this).find('span')
-	var show = function(){
-		var img = span.find('img');
-		img.velocity({opacity:1},{duration:250},[0.165, 0.84, 0.44, 1])
-	}
-	span.velocity({display:'inline-flex',width:0,opacity:0},{duration:50})
-	.velocity({width:90,opacity:1},{duration:250,complete:show},[0.165, 0.84, 0.44, 1])
-}, function() {
-	var span = $(this).find('span')
-	var hide = function(){
-		var img = span.find('img');
-		img.velocity({opacity:0},{duration:250},[0.165, 0.84, 0.44, 1])
-	}
-	span.velocity({width:0,opacity:0},{duration:250,complete:hide},[0.165, 0.84, 0.44, 1])
-	.velocity({display:'none',width:0,opacity:0},{duration:50})
 
+
+$(".direction-product a").click(function(event) {
+	event.preventDefault();
 });
 
 $(".item-hero-carousel .wrapper-img-hero").bind('mousemove', function(e) {
